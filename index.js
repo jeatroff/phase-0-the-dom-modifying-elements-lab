b08c8a1300a8a12e25dbee09e23cc8398d5d3adc
Jonathan Eatroff <jonathan.eatroff@gmail.com>
2022-11-22 16:54:56 -0500

    commit tree db9385c916d4e915a921467333b94e8d80cefb2a

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const body = document.getElementsByTagName('body')[0]
+const main = document.querySelector('main#main')
+body.removeChild(main)
+
+let newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+newHeader.textContent = 'Jonathan is the champion'
+body.append(newHeader)
